a0f8805622f2bebfd6e2e894653c2a3a08942fed
techiemaverick <vishnugs@gmail.com>
2024-12-05 03:34:33 -0500

    Messed up road runner config because of dead wheel change

--- TeamCode/src/main/java/org/coding/cobra/config/MecanumDriveConfig.java
+++ TeamCode/src/main/java/org/coding/cobra/config/MecanumDriveConfig.java
@@ -38,7 +38,7 @@ public class MecanumDriveConfig {
 
     // drive model parameters
     public double  inPerTick = 0.002; //24000 for 48 inches
-    public   double lateralInPerTick = 0.0013929811062764618;
+    public   double lateralInPerTick = 0.0013298353671469072;
     //public double lateralInPerTick = 0.0006628730045191881;
     public double trackWidthTicks =  8000;//7800;//7250; // measred //7886.722580031681;  //0
 
@@ -50,31 +50,31 @@ public class MecanumDriveConfig {
     }
 
     public static class ThreeWheelOdoParams {
-        public double par0YTicks = -2562;//-2276.11;//-5.125 * 1/ 0.002; // y position of the first parallel encoder (in tick units)
-        public double par1YTicks = 2800;//2866.59;//5.125 * 1/ 0.002; // y position of the second parallel encoder (in tick units)
-        public double perpXTicks = -2875;//-2529.56;//-7.125* 1 / 0.002; //-5.75  // x position of the perpendicular encoder (in tick units)
+        public double par0YTicks = -2276;//-2276.11;//-5.125 * 1/ 0.002; // y position of the first parallel encoder (in tick units)
+        public double par1YTicks = 2866;//2866.59;//5.125 * 1/ 0.002; // y position of the second parallel encoder (in tick units)
+        public double perpXTicks = -2869.9991971076024;//-2529.56;//-7.125* 1 / 0.002; //-5.75  // x position of the perpendicular encoder (in tick units)
     }
 
     // feedforward parameters (in tick units)
-    public double kS =  1.158509924;
-    public double kV =  0.00037828605;
+    public double kS =  1.068904792638146;
+    public double kV =  0.00037897094136720783;
     public double kA = 0.0001; //0.0001
 
     // path profile parameters (in inches)
-    public double maxWheelVel = 60;              // 15  //45
-    public double minProfileAccel = -40;        //  -10    //-30
-    public double maxProfileAccel = 60;         // 15   //45
+    public double maxWheelVel = 50;              // 15  //45
+    public double minProfileAccel = -50;        //  -10    //-30
+    public double maxProfileAccel = 50;         // 15   //45
 
     // turn profile parameters (in radians)
     public double maxAngVel = Math.PI; // shared with path
     public double maxAngAccel = Math.PI;
 
     // path controller gains
-    public double axialGain = 3;
-    public double lateralGain = 5;//25
-    public double headingGain = 3;//5; // shared with turn//25
+    public double axialGain = 0;
+    public double lateralGain = 3;//25
+    public double headingGain = 0.8; // responsible for drift to right correction //5; // shared with turn//25
 
-    public double axialVelGain = 0.0;
-    public double lateralVelGain = 0.0;
-    public double headingVelGain = 0.0; // shared with turn
+    public double axialVelGain = 0.5;
+    public double headingVelGain = 1; // shared with turn
+    public double lateralVelGain = 0.5;
 }
